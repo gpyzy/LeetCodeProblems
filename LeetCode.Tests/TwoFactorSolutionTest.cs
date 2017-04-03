617f0cc94ded7a384ba9452c917ef31d9ac236c2
Jony <jony@live.cn>
2017-04-03 13:03:53 +0800

    Initial check-in of module LeetCode

---
+++ LeetCode.Tests/TwoFactorSolutionTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+namespace LeetCode.Tests
+{
+	[TestFixture()]
+	public class TwoFactorSolutionTest
+	{
+		[Test()]
+		[TestCase(new int[] { 2, 7, 11, 15 }, 9)]
+		[TestCase(new int[] { -1, 7, 0, 15, 2, 9, -2 }, 9)]
+		public void Solution1_FindTwoNumber_Found(int[] numbers, int target)
+		{
+			int[] indicies = new int[0];
+
+			var result = TwoSumSolution.Solution1(numbers, target, ref indicies);
+
+			Assert.True(result);
+			Assert.True(indicies.Length == 2);
+			Assert.True(numbers[indicies[0]] + numbers[indicies[1]] == target);
+		}
+
+		[Test()]
+		[TestCase(new int[] { 0 }, 0)]
+		[TestCase(new int[] { 2, 7, 11, 15 }, 10)]
+		[TestCase(new int[] { }, 0)]
+		public void Solution1_FindTwoNumber_NotFound(int[] numbers, int target)
+		{
+			int[] indicies = new int[0];
+
+			var result = TwoSumSolution.Solution1(numbers, target, ref indicies);
+			Assert.True(!result);
+
+		}
+	}
+}
---
+++ LeetCode/Solutions/TwoSumSolution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	/// <summary>
+	/// https://leetcode.com/problems/two-sum/#/description
+	/// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+	/// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+	/// </summary>
+	public static class TwoSumSolution
+	{
+		/// <summary>
+		/// Time cost O(n), space cost O(n)
+		/// </summary>
+		/// <returns>The solution1.</returns>
+		/// <param name="numbers">Numbers.</param>
+		/// <param name="target">Target.</param>
+		public static bool Solution1(int[] numbers, int target, ref int[] result)
+		{
+			if (!(numbers?.Length>2))
+			{
+				return false;
+			}
+			SortedList<int, int> dict = new SortedList<int, int>();
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				var number = numbers[i];
+
+				if (dict.ContainsKey(target - number))
+				{
+					result = new int[2];
+					result[0] = dict[target - number];
+					result[1] = i;
+					return true;
+				}
+				else
+				{
+					dict[number] = i;
+				}
+			}
+
+
+			return false;
+		}
+	}
+}
