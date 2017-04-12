3f81ea1b08a361d3daab4589cf2713d1464e030b
Jony <jony@live.cn>
2017-04-12 13:57:31 +0800

    Implement IsPalindromeSolution

---
+++ LeetCode.Tests/IsPalindromeSolutionTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode.Tests
+{
+    [TestFixture]
+    public class IsPalindromeSolutionTest
+    {
+        [Test]
+        [TestCase(11, ExpectedResult = true)]
+        [TestCase(1221, ExpectedResult = true)]
+        public bool Solution_Even_Length_True(int x)
+        {
+            return LeetCode.Solutions.IsPalindromeSolution.Solution((x));
+        }
+
+        [Test]
+        [TestCase(0, ExpectedResult = true)]
+        [TestCase(111, ExpectedResult = true)]
+        [TestCase(12321, ExpectedResult = true)]
+        public bool Solution_Odd_Length_True(int x)
+        {
+            return LeetCode.Solutions.IsPalindromeSolution.Solution((x));
+        }
+
+        [Test]
+        [TestCase(1122, ExpectedResult = false)]
+        [TestCase(-1122, ExpectedResult = false)]
+        [TestCase(123, ExpectedResult = false)]
+        [TestCase(-123, ExpectedResult = false)]
+        [TestCase(10, ExpectedResult = false)]
+        [TestCase(1000021, ExpectedResult = false)]
+        public bool Solution_False(int x)
+        {
+            return LeetCode.Solutions.IsPalindromeSolution.Solution((x));
+        }
+    }
+}
--- LeetCode/Solutions/IsPalindromeSolution.cs
+++ LeetCode/Solutions/IsPalindromeSolution.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 namespace LeetCode.Solutions
 {
-    public class IsPalindromeSolution
+    public static class IsPalindromeSolution
     {
-        public IsPalindromeSolution()
+        /// <summary>
+        /// https://leetcode.com/problems/palindrome-number/#/description
+        /// Determine whether an integer is a palindrome. Do this without extra space.
+        /// </summary>
+        /// <returns>The solution.</returns>
+        /// <param name="x">The x coordinate.</param>
+        public static bool Solution(int x)
         {
+            if (x < 0 || (x > 0 && x % 10 == 0)) return false;
+            int temp = 0;
+
+            while (temp < x)
+            {
+                temp = temp * 10 + x % 10;
+                x = x / 10;
+            }
+
+
+            if (temp == x || (temp - x * 10) / 10 == 0)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
