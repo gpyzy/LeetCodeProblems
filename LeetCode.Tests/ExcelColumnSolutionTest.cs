98588084abc0bf4d133fda083d31b8b2bb0d48c4
Jony <jony@live.cn>
2017-04-12 17:11:08 +0800

    ExcelColumnSolution

---
+++ LeetCode.Tests/ExcelColumnSolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode.Tests
+{
+    [TestFixture]
+    public class ExcelColumnSolutionTest
+    {
+        [Test]
+        [TestCase(1, ExpectedResult = "A")]
+        [TestCase(52, ExpectedResult = "AZ")]
+        [TestCase(53, ExpectedResult = "BA")]
+        [TestCase(53, ExpectedResult = "BB")]
+        public string Solution_Success(int x)
+        {
+            return LeetCode.Solutions.ExcelColumnSolution.Solution(x);
+        }
+    }
+}
---
+++ LeetCode/Solutions/ExcelColumnSolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCode.Solutions
+{
+    public static class ExcelColumnSolution
+    {
+        private static readonly char[] AToZ = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                'H', 'I', 'J', 'K', 'L', 'M', 'N' ,'O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+        public static string Solution(int x)
+        {
+            if (x < 1) return "N/A";
+
+            const int scale = 26;
+            var result = string.Empty;
+            while (x != 0)
+            {
+                var modulo = (x - 1) % scale;
+                result = AToZ[modulo] + result;
+                x = (x - modulo) / 26;
+            }
+
+            return result.ToString();
+        }
+    }
+}
